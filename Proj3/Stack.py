1e97c1103096796e2f0edf4aa5f72202866a4b44
Slapppy109 <kevinphongle@gmail.com>
2018-02-15 18:34:32 -0500

    Update Stack.py

--- Proj3/Stack.py
+++ Proj3/Stack.py
@@ -1,3 +1,4 @@
+import string
 ###################################
 # PROJECT 3 - STACK
 # Author: Kevin Le
@@ -36,35 +37,87 @@ class Stack:
         return self._size
 
     def is_empty(self):
-        if self._size = 0:
-            return True
-        return false
+        return self._size == 0
 
     def top(self):
         if self._size != 0:
-            return self._data[-1]
+            return self._data[self._size - 1]
         return None
 
     # ---------------------------Mutator Functions------------------------------
 
     def push(self, addition):
-        self._data.append(addition)
+        '''
+        Adds elements on top of the stack
+        :param addition:
+        :return: None
+        '''
+        self.grow() #Grow the stack if needed
+        self._data[self._size] = addition
+        self._size += 1
 
     def pop(self):
-        return self._data.pop()
+        '''
+        Removes the top element of the stack
+        :return: the top element of the stack
+        '''
+        popped = self._data[self._size - 1]
+        self._data[self._size - 1] = None
+        self._size -= 1
+        self.shrink() #Shrink the stack if needed
+        return popped
 
     def grow(self):
-        if self._size == self._capacity:
-            self._capacity *= 2
+        '''
+        :return: None
+        '''
+        if (self._size + 1) >= self._capacity: #check if growth needed
+            self._capacity *= 2 #change size of capacity
+            new = [None] * self._capacity
+            ind = 0
+            for elm in self._data: #copy data over to new stack
+                new[ind] = elm
+                ind += 1
+            self._data = new
+
+
 
     def shrink(self):
-        if self._capacity//2 >= self._size:
-            if self._capacity >2:
-                self._capacity /= 2
+        '''
+        :return: None
+        '''
+        if ((self._size) <= self._capacity//2) and (self._size > 2): #check if growth needed
+            self._capacity = self._capacity // 2 #change size of capacity
+            new = [None] * self._capacity
+            ind = 0
+            for elm in self._data: #copy data over to new stack
+                if ind < self._size:
+                    new[ind] = elm
+                    ind += 1
+            self._data = new
 
-
-'''
-Add doc strings here too!
-'''
 def Palindrome(phrase):
-    pass
+    if (len(phrase) == 1) or (len(phrase) == 0): #empty or length 1 strings return true for palindromes
+        return True
+    check = Stack()
+
+    # Section of code to strip string of all spaces and punctuation
+    n_phrase1 = phrase.replace(" ", "")
+    n_phrase2 = ""
+    for char in n_phrase1:
+        if char.isalnum():
+            n_phrase2 += char
+    n_phrase3 = n_phrase2
+
+    # Removing middle char in an odd palindrome
+    if len(n_phrase2) % 2 == 1:
+        mid = len(n_phrase2)//2
+        n_phrase3 = n_phrase2[:mid] + n_phrase2[mid + 1:]
+    n_phrase = n_phrase3.lower()
+
+    for char in n_phrase: #checking if one half of the string is the same as the other
+        if char == check.top():
+            check.pop()
+        else:
+            check.push(char)
+    return check.is_empty() #if stack is empty, then string is a palindrome
