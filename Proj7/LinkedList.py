2d62d84e4e0eebb5f893dea209ed25955d7c48aa
Slapppy109 <kevinphongle@gmail.com>
2018-04-12 16:38:08 -0400

    Update LinkedList.py

--- Proj7/LinkedList.py
+++ Proj7/LinkedList.py
@@ -79,6 +79,10 @@ class LinkedList:
 
 
     def __repr__(self):
+        """
+        String representation of Linked List
+        :return: string of Linked List
+        """
         output = ''
         temp_self = self.head
         while temp_self is not None:
@@ -91,29 +95,47 @@ class LinkedList:
 
 
     def append(self, key, value):
+        """
+        Push node to the end of the Linked List
+        :param key: Key of element
+        :param value: Value of element
+        :return: None
+        """
         new = HashListNode(key, value)
-        if self.head = None:
-            self.head = new
+        if self.head is None:
+            self.head = self.tail = new  # Make new node the head if Linked List empty
         else:
-            self.tail.next = new
+            self.tail.next = new  # add new node as tail of Linked List
             self.tail = new
 
     def remove(self, key):
+        """
+        Removes node in the Linked List
+        :param key: Specified Key to be removed
+        :return: None
+        """
+        if self.head is None:  # return if Linked List is empty
+            return
         prev = self.head
-        target = self.head.next
-        if prev.key == key:
+        target = self.head.next  # create prev pointer and target poitner
+        if prev.key == key:  # condition if head is wanted
             self.head = target
             return
-        while target is not None:
+        while target is not None:  # iterate through Linked List to remove node
             if target.key == key:
                 prev.next = target.next
             prev = prev.next
             target = target.next
 
     def find(self, key):
+        """
+        Find node with a specified key in the Linked list
+        :param key: Specified Key to be found
+        :return: Found node or False
+        """
         temp_self = self.head
         while temp_self is not None:
-            if temp_self.key == key:
+            if temp_self.key == key:  # found target node
                 return temp_self
             temp_self = temp_self.next
-        return False
+        return False  # return if target node does not exist
